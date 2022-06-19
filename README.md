cc7fa122d1f57893006345ef648beaa815ac9d9c
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-06-19 09:03:22 -0400

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Stock Scanner App
 
 Search ticker symbols of US/Canadian stocks such as MSFT, META, AAPL etc. and check out different graphs to understand the company valuations.
+
+![Screenshot](src/images/stock-scanner.png)
--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import { ThemeProvider } from './context';
 
+import axios from 'axios';
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
