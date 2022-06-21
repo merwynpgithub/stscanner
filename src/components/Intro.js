274a73a93144c79234285f40afee86ee3e8ca8bd
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-06-21 11:09:22 -0400

    add app title for heoku server delay

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,11 @@
 import './styles/intro.css';
 function Intro({ name }) {
+  if (name === "") 
+    return (
+      <div className="intro">
+        <h2>Loading...</h2>
+      </div>
+    );
   return (
     <div className="intro">
       <h2>{name}</h2>
